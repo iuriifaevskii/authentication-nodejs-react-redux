4b8c46db099d80855902d51ec960dfb0068e9589
Iurii Faevskii <urfaevskii@gmail.com>
2017-07-04 19:03:29 +0300

    create authentication controller with test data

---
+++ server/controllers/authentication.js
@@ -0,0 +1,15 @@
+exports.signup = function(req, res, next){
+    res.send({success:true});
+
+
+
+    //See if a user with the given email exists
+
+    //If a user with email does exist, return an error
+
+    //If a user with email dose NOT exist, create and save user record
+
+    //Respond to request indicating the user was created
+
+
+}
