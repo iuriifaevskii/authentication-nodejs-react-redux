9dfd243f61d1965f496f8853f98d008a1507b34a
Iurii Faevskii <urfaevskii@gmail.com>
2017-07-04 19:16:09 +0300

    get test email and password data(when send test json body req then in console visual code i can see it)

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,9 +1,16 @@
+const User = require('../models/user')
+
 exports.signup = function(req, res, next){
-    res.send({success:true});
+    //res.send({success:true});
 
-
+    console.log(req.body); // in postman send body json {"email":"test@example.com","password":"123"}
+    const email = req.body.email;
+    const password = req.body.password;
 
     //See if a user with the given email exists
+    User.findOne({ email: email }, function(err, existingUser){
+        
+    });
 
     //If a user with email does exist, return an error
 
