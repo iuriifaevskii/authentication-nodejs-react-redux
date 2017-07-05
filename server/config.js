851057be1b526685c0d938d2d949c43e15598eff
Iurii Faevskii <urfaevskii@gmail.com>
2017-07-05 12:52:04 +0300

    create token(after success registr I return token which composed with user id, time and my secret string)

---
+++ server/config.js
@@ -0,0 +1,5 @@
+//Hold application secrets and config
+
+module.exports = {
+    secret: 'laiejrsagdsvxcb241512' //insert in gitignore config.js because it's secret information
+}
--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user')
+const config = require('../config');
+
+function tokenForUser(user){
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next){
     //res.send({success:true});
@@ -30,7 +37,8 @@ exports.signup = function(req, res, next){
             if(err){ return next(err); }
 
             //Respond to request indicating the user was created
-            res.json({success:true});   //res.json(user); - return user in response and in postman
+            res.json({token: tokenForUser(user)});//after success registr I return token composed with user.id, time and my secret string   
+            //res.json(user); - return user in response and in postman
 
         });    //save a record in database
 
