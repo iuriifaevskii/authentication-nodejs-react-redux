452576a252897ed06352fb6b6d35a8fcb4258ef5
Iurii Faevskii <urfaevskii@gmail.com>
2017-07-05 12:29:17 +0300

    fix some bugs in generate password and comment lines

--- server/models/user.js
+++ server/models/user.js
@@ -9,16 +9,20 @@ const userSchema = new Schema({
 });
 
 // On Save Hook, encrypt password
-
+//Before saving a model, run this function
 userSchema.pre('save', function(next){
-    const user = this;
+    //get access to the user model
+    const user = this;//user.email, user.password
 
-    bcrypt.getSalt(10, function(err,salt){
+    //generate a salt then run callback
+    bcrypt.genSalt(10, function(err,salt){
         if(err){ return next(err); }
 
+        //hash (encrypt) our password using the salt
         bcrypt.hash(user.password, salt, null, function(err,hash){
             if(err){ return next(err); }
             
+            // overwrite plain text password with encrypted password
             user.password = hash;
             next();
         });
