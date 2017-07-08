7fbc128229a66c0618155995f5e9be2b25cea666
Iurii Faevskii <urfaevskii@gmail.com>
2017-07-08 09:42:29 +0300

    create header and signin components

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import Header from './header'
+
 export default class App extends Component {
   render() {
     return (
-      <div>authentication react redux</div>
+      <div>
+        <Header />
+        authentication react redux
+      </div>
     );
   }
 }
